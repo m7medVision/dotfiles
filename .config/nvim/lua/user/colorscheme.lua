9d7a20730c9f863b70f44b92d2505ec38d359522
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-07-27 13:04:12 +0400

    feat(colorscheme): add tokyonight.nvim theme

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -7,8 +7,11 @@ return {
   },
   {
     "projekt0n/github-nvim-theme",
-    config = function()
-      vim.cmd("colorscheme github_dark")
-    end,
   },
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      vim.cmd("colorscheme tokyonight-night")
+    end,
+  }
 }
