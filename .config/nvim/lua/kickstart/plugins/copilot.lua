e96b5c9f019490e0bbea5f1f6747df09e0d55a8e
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-09-25 23:21:57 +0400

    update my config

---
+++ .config/nvim/lua/kickstart/plugins/copilot.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        panel = {
+          keymap = {
+            jump_next = '<M-[>',
+            jump_prev = '<M-]>',
+            accept = '<M-a>',
+            refresh = 'r',
+            open = '<S-CR>',
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = '<M-a>',
+            next = '<M-[>',
+            prev = '<M-]>',
+            dismiss = '<M-x>',
+            accept_word = '<M-w>',
+            accept_line = '<M-e>',
+          },
+        },
+        filetypes = {
+          ['.'] = false,
+        },
+        copilot_node_command = 'node',
+      }
+
+      -- local opts = { noremap = true, silent = true }
+      -- vim.api.nvim_set_keymap("n", "<M-b>", "<cmd>Copilot panel<CR>", opts)   config = function()
+      --   end,
+      --
+    end,
+  },
+  {
+    'AndreM222/copilot-lualine',
+  },
+}
