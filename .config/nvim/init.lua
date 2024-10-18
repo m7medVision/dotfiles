2f20821b8149fb07da16b81136e1ac0524ad806b
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-18 11:46:12 +0400

    chore: update my config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -654,13 +654,13 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
+  -- require 'mohammed.plugins.debug',
+  require 'mohammed.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.copilot',
-  require 'kickstart.plugins.oil',
+  require 'mohammed.plugins.autopairs',
+  require 'mohammed.plugins.neo-tree',
+  require 'mohammed.plugins.copilot',
+  require 'mohammed.plugins.oil',
   -- require 'kickstart.plugins.blink_cmp',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
--- .config/nvim/lua/mohammed/plugins/oil.lua
+++ .config/nvim/lua/mohammed/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
   'stevearc/oil.nvim',
   opts = {},
   -- Optional dependencies
-  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('oil').setup {
       columns = {
