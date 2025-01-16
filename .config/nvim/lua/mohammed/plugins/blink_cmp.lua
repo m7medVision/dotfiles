a7c1b71868888d4285dce860e2eb7331cf080be0
Mohammed <88824957+m7medVision@users.noreply.github.com>
2025-01-16 15:00:10 +0400

    chore: edit blink_cmp config

--- .config/nvim/lua/mohammed/plugins/blink_cmp.lua
+++ .config/nvim/lua/mohammed/plugins/blink_cmp.lua
@@ -1,35 +1,77 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = {
-    'rafamadriz/friendly-snippets',
+  {
+    'saghen/blink.compat',
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = '*',
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
   },
-
-  version = '*',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = {
-      preset = nil,
-      ['<C-y>'] = { 'select_and_accept' },
-      ['<C-space>'] = { 'show', 'accept' },
-      ['<C-c>'] = { 'hide' },
-      ['<up>'] = { 'select_prev' },
-      ['<down>'] = { 'select_next' },
+  {
+    'saghen/blink.cmp',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
     },
 
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono',
-    },
-    completion = {
-      ghost_text = {
+    version = '*',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      appearance = {
+        use_nvim_cmp_as_default = true,
+      },
+      completion = {
+        trigger = {
+          -- When true, will show the completion window after typing a trigger character
+          show_on_trigger_character = true,
+          -- When both this and show_on_trigger_character are true, will show the completion window
+          -- when the cursor comes after a trigger character when entering insert mode
+          show_on_insert_on_trigger_character = true,
+          -- List of trigger characters (on top of `show_on_blocked_trigger_characters`) that won't trigger
+          -- the completion window when the cursor comes after a trigger character when
+          -- entering insert mode/accepting an item
+          show_on_x_blocked_trigger_characters = { "'", '"', '(', '{' },
+          -- or a function, similar to show_on_blocked_trigger_character
+        },
+        menu = {
+          border = 'rounded',
+          draw = {
+            columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind', gap = 1 } },
+            treesitter = {},
+          },
+        },
+
+        -- experimental auto-brackets support
+        accept = {
+          auto_brackets = { enabled = false },
+        },
+
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+          treesitter_highlighting = true,
+          window = {
+            border = 'rounded',
+          },
+        },
+
+        ghost_text = {
+          enabled = false,
+        },
+      },
+
+      signature = {
         enabled = true,
+        window = {
+          border = 'rounded',
+        },
       },
     },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
+    opts_extend = { 'sources.default' },
   },
-  opts_extend = { 'sources.default' },
 }
