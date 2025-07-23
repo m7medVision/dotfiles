7d3a0d217ce5fee793cdf2f1f7d0dc92150b30ac
Mohammed <88824957+m7medVision@users.noreply.github.com>
2025-07-23 10:05:34 +0400

    feat: change the theme

--- .config/nvim/lua/mohammed/plugins/theme.lua
+++ .config/nvim/lua/mohammed/plugins/theme.lua
@@ -1,17 +1,52 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  opts = {
+    term_colors = true,
+    transparent_background = true,
+    dim_inactive = {
+      enabled = false, -- dims the background color of inactive window
+      shade = 'dark',
+      percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      treesitter = true,
+      harpoon = true,
+      telescope = true,
+      mason = true,
+      noice = true,
+      notify = true,
+      which_key = true,
+      fidget = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { 'italic' },
+          hints = { 'italic' },
+          warnings = { 'italic' },
+          information = { 'italic' },
+        },
+        underlines = {
+          errors = { 'underline' },
+          hints = { 'underline' },
+          warnings = { 'underline' },
+          information = { 'underline' },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+      mini = {
+        enabled = true,
+        indentscope_color = '',
+      },
+    },
+  },
+  config = function(_, opts)
+    require('catppuccin').setup(opts)
+    vim.cmd.colorscheme 'catppuccin-macchiato'
   end,
 }
