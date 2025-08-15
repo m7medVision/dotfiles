577f0b0eac7eb84450e1ee472e043ca1844e4a78
Mohammed <88824957+m7medVision@users.noreply.github.com>
2025-08-15 10:11:29 +0400

    feat: add unlock using a key

--- .config/hypr/bin/face_check_hyprlock.sh
+++ .config/hypr/bin/face_check_hyprlock.sh
@@ -1,12 +1,15 @@
 
 #!/bin/bash
-if pgrep -x hyprlock >/dev/null; then
+if pgrep -x hyprlock > /dev/null; then
     cd ~/face_check/
     ./venv/bin/python -W ignore::UserWarning face_recognition_ir.py
     if [ $? -eq 0 ]; then
         exit 0
     else
-        loginctl unlock-session
+        pkill -USR1 hyprlock
         exit 1
     fi
+else
+    echo "Hyprlock is not running."
+    exit 1
 fi
