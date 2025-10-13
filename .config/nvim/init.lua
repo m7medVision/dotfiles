9caab457b1a9dcabf9f99837433c9dcbc502b1cb
Mohammed <88824957+m7medVision@users.noreply.github.com>
2025-10-13 19:12:43 +0400

    feat: better lazy.nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,13 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -17,24 +17,25 @@ vim.opt.rtp:prepend(lazypath)
 
 require 'mohammed.common'
 
-require("lazy").setup({
+require('lazy').setup {
   spec = {
-    { import = "mohammed.plugins" },
+    { import = 'mohammed.plugins' },
   },
-  install = { colorscheme = { "habamax" } },
-  checker = { enabled = true },
+  install = { colorscheme = { 'habamax' } },
+  checker = { enabled = false },
+  change_detection = { enabled = true },
   performance = {
     rtp = {
       disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
       },
     },
   },
-})
+}
