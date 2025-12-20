7398c5bc07e111f8c169026b7707edd741bea11e
Mohammed <88824957+m7medVision@users.noreply.github.com>
2025-12-20 20:53:14 +0400

    feat: add next edit suggest

--- .config/nvim/lua/mohammed/plugins/copilot_sidekick.lua
+++ .config/nvim/lua/mohammed/plugins/copilot_sidekick.lua
@@ -4,8 +4,24 @@ return {
     branch = 'release',
     cmd = 'Copilot',
     event = 'BufReadPost',
-    -- TODO: config copilot mappings
     config = function() end,
+  },
+  {
+    'copilotlsp-nvim/copilot-lsp',
+    init = function()
+      vim.g.copilot_nes_debounce = 500
+      vim.lsp.enable 'copilot_ls'
+      vim.keymap.set('n', '<tab>', function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local state = vim.b[bufnr].nes_state
+        if state then
+          local _ = require('copilot-lsp.nes').walk_cursor_start_edit() or (require('copilot-lsp.nes').apply_pending_nes() and require('copilot-lsp.nes').walk_cursor_end_edit())
+          return nil
+        else
+          return '<C-i>'
+        end
+      end, { desc = 'Accept Copilot NES suggestion', expr = true })
+    end,
   },
   {
     'folke/sidekick.nvim',
