26408439ec2b64b47f9fb4efcdfa0ac252e4f1a2
Mohammed <88824957+m7medVision@users.noreply.github.com>
2025-12-20 19:51:32 +0400

    add to do tag

--- .config/nvim/lua/mohammed/plugins/copilot_sidekick.lua
+++ .config/nvim/lua/mohammed/plugins/copilot_sidekick.lua
@@ -4,14 +4,8 @@ return {
     branch = 'release',
     cmd = 'Copilot',
     event = 'BufReadPost',
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.keymap.set('i', '<M-a>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false,
-        silent = true,
-      })
-    end,
+    -- TODO: config copilot mappings
+    config = function() end,
   },
   {
     'folke/sidekick.nvim',
