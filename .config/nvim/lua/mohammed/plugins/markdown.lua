5635eb272bc48995d54238c2b54b222a5d3c585e
Mohammed <88824957+m7medVision@users.noreply.github.com>
2026-01-17 22:07:57 +0400

    fix(nvim): remove copilot-chat from markdown plugin file types

--- .config/nvim/lua/mohammed/plugins/markdown.lua
+++ .config/nvim/lua/mohammed/plugins/markdown.lua
@@ -1,8 +1,7 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  ft = { "markdown", "copilot-chat" },
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
   opts = {
-    file_types = { 'markdown', 'copilot-chat' },
+    file_types = { 'markdown' },
   },
 }
