d3dcce7c0f265036c51d8115149f4cb2459a2e37
Mohammed <88824957+m7medVision@users.noreply.github.com>
2026-01-18 11:43:10 +0400

    feat(treesitter): enable additional_vim_regex_highlighting for markdown in highlight

--- .config/nvim/lua/mohammed/plugins/lsp.lua
+++ .config/nvim/lua/mohammed/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
         ensure_installed = {},
         ignore_install = {},
         auto_install = true,
-        highlight = { enable = true },
+        highlight = { enable = true, additional_vim_regex_highlighting = { 'markdown' } },
         indent = { enable = true },
       }
     end,
@@ -126,5 +126,67 @@ return {
   {
     'nvim-lua/plenary.nvim',
     lazy = true,
+  },
+
+  -- Image support for Neovim (requires Kitty terminal with graphics protocol)
+  {
+    '3rd/image.nvim',
+    build = false, -- so that it doesn't build the rock
+    opts = {
+      backend = 'kitty',
+      processor = 'magick_cli',
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { 'markdown', 'vimwiki' },
+        },
+        neorg = {
+          enabled = true,
+          filetypes = { 'norg' },
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+    },
+  },
+
+  -- Diagram rendering for markdown and neorg
+  {
+    '3rd/diagram.nvim',
+    dependencies = { '3rd/image.nvim' },
+    opts = {
+      events = {
+        render_buffer = { 'InsertLeave', 'BufWinEnter', 'TextChanged' },
+        clear_buffer = { 'BufLeave' },
+      },
+      renderer_options = {
+        mermaid = {
+          theme = nil,
+          scale = 1,
+        },
+        plantuml = {
+          charset = nil,
+        },
+        d2 = {
+          theme_id = nil,
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>dp',
+        function()
+          require('diagram').show_diagram_hover()
+        end,
+        mode = 'n',
+        ft = { 'markdown', 'norg' },
+        desc = 'Show diagram in new tab',
+      },
+    },
   },
 }
