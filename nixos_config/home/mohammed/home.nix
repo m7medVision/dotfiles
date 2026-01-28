49f62f670c860455ce35ebf37fdf3b632c6f5c9d
Mohammed <88824957+m7medVision@users.noreply.github.com>
2026-01-28 10:31:27 +0400

    chore: add some pkgs

--- nixos_config/home/mohammed/home.nix
+++ nixos_config/home/mohammed/home.nix
@@ -11,7 +11,8 @@
     lazygit
     neovim     # Basic install for your Lua config
     bitwarden-desktop
-    opencode   
+    opencode
+
     # Utilities
     nvtopPackages.full # GPU monitoring
     htop
@@ -19,13 +20,37 @@
     curl
     wget
     unzip
+    zsh
+    tmux
+    tree-sitter
+    # Dependencies for scripts and tools
+    kitty
+    fontconfig
+    gnome-calculator
+    pavucontrol
+    networkmanagerapplet
+    brightnessctl
+    pamixer
+    playerctl
+    nodejs_22
+    # Theme management
+    glib # for gsettings
   ];
+
+  home.file.".config/nvim" = {
+    source = ../../../.config/nvim;
+    recursive = true;  # Copies instead of symlinks, but keeps your config intact
+  };
+  home.file.".config/lazygit".source = ../../../.config/lazygit;
+  home.file.".config/opencode".source = ../../../.config/opencode;
+  home.file.".tmux.conf".source = ../../../.tmux.conf;
+  home.file.".zshrc".source = ../../../.zshrc;
 
   # Basic Git Config
   programs.git = {
     enable = true;
     userName = "Mohammed";
-    userEmail = ""; # Update this
+    userEmail = "88824957+m7medVision@users.noreply.github.com";
   };
 
   # Let Home Manager install and manage itself.
