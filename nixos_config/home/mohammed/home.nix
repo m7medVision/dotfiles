fbee412e46fda61fba1a71041029b5acf36bbbcd
Mohammed <88824957+m7medVision@users.noreply.github.com>
2026-01-29 10:48:05 +0400

    fix: config gh-cli using home manger

--- nixos_config/home/mohammed/home.nix
+++ nixos_config/home/mohammed/home.nix
@@ -12,7 +12,6 @@
     neovim-unwrapped
     bitwarden-desktop
     opencode
-    gh # github
 
     # Utilities
     wl-clipboard
@@ -45,7 +44,6 @@
 
   home.file.".config/nvim" = {
     source = ../../../.config/nvim;
-    recursive = true;  # Copies instead of symlinks, but keeps your config intact
   };
   home.file.".config/lazygit".source = ../../../.config/lazygit;
   home.file.".tmux.conf".source = ../../../.tmux.conf;
@@ -58,6 +56,13 @@
     settings.user.email = "88824957+m7medVision@users.noreply.github.com";
   };
 
+  programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "https";    # preferred protocol e.g., "ssh" or "https"
+        editor = "nano";         # default editor for gh prompts
+    };
+  };
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
