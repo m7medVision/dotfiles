e9f1c064d18e6d6d752af2b61ba08106b1081f00
Mohammed <88824957+m7medVision@users.noreply.github.com>
2026-01-30 23:05:27 +0400

    update opencode.nvim lua

--- .config/nvim/lua/mohammed/plugins/opencode.lua
+++ .config/nvim/lua/mohammed/plugins/opencode.lua
@@ -10,8 +10,8 @@ return {
     vim.g.opencode_opts = {
       -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
     }
-    vim.keymap.set({ "n", "x" }, "<C-p>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+    vim.keymap.set({ "n", "t" }, "<C-p>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+    vim.keymap.set({ "n", "x" }, "<C-i>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
     vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
     vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
     -- Required for `opts.events.reload`.
