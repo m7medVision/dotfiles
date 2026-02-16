8eb58f4d857efd8f0c779ab7af867b0255201e40
Mohammed <88824957+m7medVision@users.noreply.github.com>
2026-02-16 11:12:50 +0400

    feat(tmux): switch pane navigation keybindings from arrow keys to hjkl

--- .config/nvim/lua/mohammed/plugins/vimtmux.lua
+++ .config/nvim/lua/mohammed/plugins/vimtmux.lua
@@ -1,14 +1,14 @@
 return {
   "aserowy/tmux.nvim",
   keys = {
-    { "<M-left>", function() require('tmux').resize_left() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux resize left" },
-    { "<M-down>", function() require('tmux').resize_bottom() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux resize down" },
-    { "<M-up>", function() require('tmux').resize_top() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux resize up" },
-    { "<M-right>", function() require('tmux').resize_right() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux resize right" },
-    { "<C-left>", function() require('tmux').move_left() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux move left" },
-    { "<C-down>", function() require('tmux').move_bottom() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux move down" },
-    { "<C-up>", function() require('tmux').move_top() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux move up" },
-    { "<C-right>", function() require('tmux').move_right() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux move right" },
+    { "<M-h>", function() require('tmux').resize_left() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux resize left" },
+    { "<M-j>", function() require('tmux').resize_bottom() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux resize down" },
+    { "<M-k>", function() require('tmux').resize_top() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux resize up" },
+    { "<M-l>", function() require('tmux').resize_right() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux resize right" },
+    { "<C-h>", function() require('tmux').move_left() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux move left" },
+    { "<C-j>", function() require('tmux').move_bottom() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux move down" },
+    { "<C-k>", function() require('tmux').move_top() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux move up" },
+    { "<C-l>", function() require('tmux').move_right() end, mode = { 'n', 'i', 't', 'v', 's', 'x', 'o', 'c' }, desc = "tmux move right" },
   },
   config = function()
     require('tmux').setup {
