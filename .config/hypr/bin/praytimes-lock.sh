eae0ba1032bf04cb97e1b28a0107fb4b07257272
Mohammed <88824957+m7medVision@users.noreply.github.com>
2026-03-16 22:43:08 +0400

    remove stuff that's not attractive anymore
    I not kid anymore I have to work not only tweaking my config sor0ry niri
    and hyprland, it was great time

--- .config/hypr/bin/praytimes-lock.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Get all prayer times for lock screen display in Arabic
-python3 /home/mohammed/.config/Scripts/praytime.py --lockscreen 2>/dev/null | sed 's/Fajr/الفجر/g; s/Sunrise/الشروق/g; s/Dhuhr/الظهر/g; s/Asr/العصر/g; s/Maghrib/المغرب/g; s/Isha/العشاء/g' || echo "🕌 جاري تحميل أوقات الصلاة..."
